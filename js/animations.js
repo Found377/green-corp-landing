9ed8314a64a6973533493b972a6741d1dc16687c
Found377 <found377@gmail.com>
2022-10-03 22:57:09 +0300

    Добавил анимацию и варианты для бюджета 100к+

--- js/animations.js
+++ js/animations.js
@@ -1,3 +1,5 @@
+/* Анимация числа счастливых клиентов */
+
 const INCREASE_NUMBER_ANIMATION_SPEED = 50;
 
 function increaseNumberAnimationStep(i, element, endNumber) {
@@ -7,7 +9,45 @@ function increaseNumberAnimationStep(i, element, endNumber) {
     } else {
       element.innerText = i;
     }
-    i++;
+
+    i += 100;
+
+    setTimeout(function () {
+      increaseNumberAnimationStep(i, element, endNumber);
+    }, INCREASE_NUMBER_ANIMATION_SPEED);
   }
-  function setTimeOut() {}
 }
+
+function initIncreaseNumberAnimation() {
+  const element = document.querySelector(".features__clients-count");
+
+  increaseNumberAnimationStep(0, element, 5000);
+}
+
+initIncreaseNumberAnimation();
+
+/* Добавление варианта выбора конкретного бюджета более 100к */
+
+document
+  .querySelector("#budget")
+  .addEventListener("change", function handleSelectChange(event) {
+    if (event.target.value === "other") {
+      let formContainer = document.createElement("div");
+      formContainer.classList.add("form__group");
+      formContainer.classList.add("form__other-input");
+
+      let input = document.createElement("input");
+      input.placeholder = "Введите Ваш вариант";
+      input.type = "text";
+
+      formContainer.appendChild(input);
+      document
+        .querySelector("#form form")
+        .insertBefore(formContainer, document.querySelector(".form__submit"));
+    }
+
+    let otherInput = document.querySelector(".form__other-input");
+    if (event.target.value !== "other" && Boolean(otherInput)) {
+      document.querySelector("#form form").removeChild(otherInput);
+    }
+  });
