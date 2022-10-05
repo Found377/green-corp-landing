4775292f3c4404f298af1cbf22be2b6f2466f165
Found377 <found377@gmail.com>
2022-10-05 23:50:37 +0300

    Добавил анимацию плавного перехода по разделам

--- js/animations.js
+++ js/animations.js
@@ -72,3 +72,29 @@ function updateScroll() {
 }
 
 window.addEventListener("scroll", updateScroll);
+
+/* Добавление плавной анимации перехода по разделам */
+
+function addSmoothScroll(anchor) {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+}
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  addSmoothScroll(anchor);
+});
+
+function onLinkClick(event) {
+  event.preventDefault();
+
+  document.querySelector(event.target.getAttribute("href")).scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
+addSmoothScroll(document.querySelector(".more-button"));
